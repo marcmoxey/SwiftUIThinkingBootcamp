eed55d52deb5d1e630e33d4637a60530c9018a35
marcmoxey <moxeymarc@gmail.com>
2026-01-07 14:50:32 -0500

    Learned enums

--- SwiftBasics/Enums.playground/Contents.swift
+++ SwiftBasics/Enums.playground/Contents.swift
@@ -1,3 +1,57 @@
 import UIKit
 
+// Enums is the same as Struct expect we know all cases at run time
+
+
+struct CarModel {
+    let brand: CarBrandOption
+    let model: String
+    
+}
+
+struct CarBrand {
+    let title: String
+}
+
+// Enums are stored in memry the same way as a Struct but we cannnot mutaute them
+enum CarBrandOption {
+    case ford
+    case toyota
+    case honda
+    
+    
+    var title: String {
+        
+        switch self {
+        case .ford:
+            return "Ford"
+        case .toyota:
+            return "Toyota"
+        case .honda:
+            return "Honda"
+        }
+    }
+ 
+}
+
+//var car1: CarModel = CarModel(brand: "Ford", model: "Fiesta")
+//var car2: CarModel = CarModel(brand: "Ford", model: "Focus")
+//var car3: CarModel = CarModel(brand: "Toyota", model: "Camry")
+
+var brand1 = CarBrand(title: "Ford")
+var brand2 = CarBrand(title: "Toyota")
+
+
+//var car1 = CarModel(brand: brand1, model: "Fiesta")
+//var car2 = CarModel(brand: brand1, model: "Ford")
+//var car3 = CarModel(brand: brand2, model: "Camry")
+
+
+var car1 = CarModel(brand: .ford, model: "Fiesta")
+var car2 = CarModel(brand: .ford, model: "Focus")
+var car3 = CarModel(brand: .toyota, model: "Camry")
+
+var fordBrand: CarBrandOption = .ford
+print(fordBrand.title)
+
 
