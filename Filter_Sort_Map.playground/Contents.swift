cf73c89ba41be86d2330ad1a4cdfbb00efdf2fe7
marcmoxey <moxeymarc@gmail.com>
2026-01-08 11:03:47 -0500

    Learned Maps, Filter, Sort

--- Filter_Sort_Map.playground/Contents.swift
+++ Filter_Sort_Map.playground/Contents.swift
@@ -1,2 +1,62 @@
 import UIKit
 
+
+struct DatabaseUser {
+    let name: String
+    let isPremium: Bool
+    let order: Int
+}
+
+
+var allUsers: [DatabaseUser] = [
+    DatabaseUser(name: "Marc", isPremium: true, order: 28),
+    DatabaseUser(name: "Nick", isPremium: true, order: 4),
+    DatabaseUser(name: "Emily", isPremium: false, order: 1),
+    DatabaseUser(name: "Samantha", isPremium: true, order: 3),
+    DatabaseUser(name: "Joe", isPremium: true, order: 10000),
+    DatabaseUser(name: "Chirs", isPremium: false, order: 2),
+]
+//
+//var allPremiumUsers: [DatabaseUser] = allUsers.filter { user in
+//    if user.isPremium {
+//        return true
+//    }
+//    
+//    return false
+//}
+
+
+//for user in allUsers {
+//    if user.isPremium {
+//        allPremiumUsers.append(user)
+//    }
+//}
+
+
+var allPremiumUsers: [DatabaseUser] = allUsers.filter { user in
+    return user.isPremium
+}
+
+var allPremiumUsers2: [DatabaseUser] = allUsers.filter({ $0.isPremium})
+
+
+print(allPremiumUsers)
+    
+
+
+var orderUsers: [DatabaseUser] = allUsers.sorted { user1, user2 in
+    return user1.order < user2.order
+}
+
+var orderUsers2: [DatabaseUser] = allUsers.sorted(by: { $0.order < $1.order })
+
+
+print(orderUsers)
+
+var userNames: [String] = allUsers.map { user in
+    return user.name
+}
+
+var userName2: [String] = allUsers.map({ $0.name})
+
+print(userNames)
